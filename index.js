e952f7d8042e431019d727c656049d5a737b274a
TalyssonOC <a11023@bcc.unifal-mg.edu.br>
2014-12-17 10:54:10 -0200

    Improved loading

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ var patterns = {};
 
 fs.readdirSync(path.join(__dirname, 'patterns')).forEach(function(pattern) {
 	var patternPath = path.join(__dirname, 'patterns/' + pattern);
+	var name = path.basename(pattern, '.js');
 
-	patterns[path.basename(pattern, '.js')] = {
-		template: fs.readFileSync(patternPath).toString(),
+	patterns[name] = {
 		path: patternPath
 	};
+
+	Object.defineProperty(patterns[name], 'template', {
+		get: function() {
+			if(typeof this._source === 'undefined') {
+				this._source = fs.readFileSync(patternPath).toString();
+			}
+
+			return this._source;
+		}
+	});
 });
 
 module.exports = patterns;
